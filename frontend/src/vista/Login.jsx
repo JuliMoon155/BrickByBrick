701273f2018fe23a3c9e6ffa9cefd75999e4e9c2
Maycol <sfuquene20@gmail.com>
2024-09-18 19:29:28 -0500

    creacion rama login y registro

---
+++ frontend/src/vista/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='contenedorPrincipal'>
+      <div className='login'>
+      hola
+      </div>
+      <div className="camino_registro">
+      adios
+      </div>
+    </div>
+  )
+}
+
+export default Login
