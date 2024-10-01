6596c2a84fb08f6a7973077d48cdedb0ada1d65e
Juan Garcia <spdjuanse@gmail.com>
2024-09-30 23:38:57 -0500

    Arreglo interfaz y comprobacion de valores en publicacion contenido

--- frontend/src/components/PublicarMaterial.jsx
+++ frontend/src/components/PublicarMaterial.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import FormularioPublicacionMateriales from "./FormularioPrecentacionMateriales";
 import VistaPreviewMateriales from "./PreViewPublicacionMateriales";
 import "../styles/PublicarMaterial.css";
+import { Header } from './Header';
 
 export const PublicacionMateriales = ({ userId, usuario }) => {
   const [material, setMaterial] = useState({
@@ -17,19 +18,22 @@ export const PublicacionMateriales = ({ userId, usuario }) => {
   };
 
   return (
-    <div className="contenedor-publicacion">
-      <div className="contenido-publicacion">
-        <div className="seccion-formulario">
-          <FormularioPublicacionMateriales
-            material={material}
-            onCambioMaterial={manejarCambioMaterial}
-            usuario={usuario}
-          />
-        </div>
-        <div className="seccion-vista-previa">
-          <VistaPreviewMateriales material={material} userId={userId} />
+    <>
+      <Header />
+      <div className="contenedor-publicacion">
+        <div className="contenido-publicacion">
+          <div className="seccion-formulario">
+            <FormularioPublicacionMateriales
+              material={material}
+              onCambioMaterial={manejarCambioMaterial}
+              usuario={usuario}
+            />
+          </div>
+          <div className="seccion-vista-previa">
+            <VistaPreviewMateriales material={material} userId={userId} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
