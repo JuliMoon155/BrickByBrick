82d9984f4edad5ab031bf5e3f4101b4623c460af
Maycol <sfuquene20@gmail.com>
2024-09-30 19:22:37 -0500

    cambio en VistaPreviaMaterial

--- frontend/src/components/VistaPreviaMaterial.jsx
+++ frontend/src/components/VistaPreviaMaterial.jsx
@@ -1,17 +1,52 @@
 import '../styles/VisualizarMaterial.css';
-import PropTypes from "prop-types";
+import '../styles/HeaderPrincipal.css';
+import '../styles/detalleMatConst.css';
+//import 
+import PropTypes, { func } from "prop-types";
+import Popup from 'reactjs-popup';
 
 function VistaPreviaMaterial({ material }) {
-    let onClick = () => {
-
-    };
+    // let vistaPrevMat = () => {
+    //     console.log("Si entra a la function");
+    // };
 
     return (
-        <div className="contenedor-vista-previa" onClick={onClick}>
-            <img src={material.urlFoto} alt="foto"/>
+        <div className="contenedor-vista-previa">
+            <img src={material.urlFoto} alt="foto"/>    
             <div className="contenedor-vista-previa-informacion">
-                <h2 className="titulo-material">{material.titulo}</h2>
-                <p>{material.descripcion}</p>
+                <h2 className="titulo-material">{material.titulo}</h2>  
+                {/* <p>{material.de scripcion}</p> */}
+                <Popup
+                trigger={open => (
+                <button className='botondelPopUp'>VER DETALLES</button>)}
+                position="right center"
+                closeOnDocumentClick>
+                    <div className="container-popup">
+                        <div className="material">
+                          <div className="info">
+                            <div className="title">
+                              <h1>NOMBRE DEL MATERIAL</h1>
+                            </div>
+                            <div className="cant">
+                              <p>Cantidad:<br />70KG</p>
+                            </div>
+                            <div className="status">
+                              <p>Estado:<br />Re bueno</p>
+                            </div>
+                            <div className="description">
+                              <p>Descripción:<br />{material.descripcion}</p>
+                            </div>
+                            <div className="btn-detalle-material">
+                              <center><button className="detalle-material-btn">Inscribirse al evento</button>
+                              </center>
+                            </div>
+                          </div>
+                          {/* <div className="image">
+                            <center><img src= {material.urlFoto} /></center>
+                          </div> */}
+                        </div>
+                    </div>
+                </Popup>
             </div>
         </div>
     );
@@ -22,6 +57,8 @@ VistaPreviaMaterial.propTypes = {
         urlFoto: PropTypes.string,
         titulo: PropTypes.string,
         descripcion: PropTypes.string,
+        cantidad: PropTypes.string,
+        estado: PropTypes.string,
     }),
     onClick: PropTypes.func,
 }
