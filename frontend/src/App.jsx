fb8bd3efa1179c4c88bc2085fb8e40f662286383
Juliana <jumemu2002@hotmail.com>
2024-11-05 19:38:15 -0500

    add perfil, id en publicaciones, autoupdate publicaciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import { Login } from './components/Login';
 import { PublicacionMateriales } from './components/PublicarMaterial';
 import { Perfil } from './components/Perfil';
 import { HomePage } from './components/HomePage';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BusquedaEvento from "./components/BusquedaEvento";
+import { Contenido } from './components/Contenido';  
 
 function App() {
   const [interfaz, setInterfaz] = useState('Login');
   const [userId, setUserId] = useState(null);
-  const [usuario, setUsuario] = useState(null);
+  const [userUsuario, setUserUsuario] = useState(null);
   const [userRol, setUserRol] = useState(null);
 
   const cambiarInterfaz = (nuevaInterfaz) => {
@@ -18,9 +19,8 @@ function App() {
 
   const handleLoginSuccess = (id, rol, usuario) => {
     setUserId(id);
-    setUsuario(usuario);
+    setUserUsuario(usuario);
     setUserRol(rol);
-    console.log(id+" id"+usuario+" usuario"+rol+" rol");
     if (rol === 'Beneficiario') { 
       cambiarInterfaz('HomePage');
     } else if (rol === 'Empresario'){
@@ -40,13 +40,17 @@ function App() {
         <Registro onLogin={() => cambiarInterfaz('Login')} />
       )}
       {interfaz === 'HomePage' && (
-        <HomePage /> 
-      )} 
+        <HomePage usuario={userUsuario} userId={userId} userRol={userRol} /> 
+      )}
       {interfaz === 'PublicacionMateriales' && (
-      <PublicacionMateriales usuario={usuario} userId={userId}/> 
+      <PublicacionMateriales usuario={userUsuario} userId={userId}/> 
       )} 
       {interfaz === 'BusquedaEvento' && (
         <BusquedaEvento/>
+      )}
+      {/* Paso el userId a Contenido */}
+      {interfaz === 'Contenido' && (
+        <Contenido usuario={userUsuario} userId={userId} />
       )}
     </>
   );
--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,34 +2,42 @@ import React, { useEffect, useState } from 'react';
 import { Header } from './Header';
 import { Contenido } from './Contenido';
 import  '../styles/HomePage.css';
+import profileDefault from '../imgTemp/profileDefault.png';
 
-export const HomePage = () => {
 
-  const [contenido_foryou, setContenido_foryou] = useState(<Contenido />);
-/*
-  const handleContenidoForyou = (nombre) => {
-    if (nombre=='Contenido') {
-      setContenido_foryou(<Contenido />);
-    }if(nombre=='Donaciones') {
-      console.log('se cambio');
-    }
-  };
-  */
+export const HomePage = ({ userId, usuario, userRol}) => {
+    const [contenido_foryou, setContenido_foryou] = useState(<Contenido userId={userId} usuario={usuario} />);
+  
+    useEffect(() => {
+      setContenido_foryou(<Contenido userId={userId} usuario={usuario} />);
+    }, [userId, usuario]);
+
   return (
     <div className='HomePage'>
       <Header />
       <div className='Contenido'>
-        <div className='perfil'>
-          <div className='infoPersonal'></div>
-          <div className='otraInfo'></div>
+      <div className='perfil'>
+          <div className='infoPersonal'>
+              <img src={profileDefault} alt="Imagen de perfil" className='fotoPerfil' />
+              <h3 className="nombreUsuario">@{usuario}</h3>
+              <h3 className="rolUsuario">{userRol}</h3>
+          </div>
+          <div className='otraInfo'>
+            <h3 className="titCambiarInfo">Actualizar Datos</h3>
+            <h4 className="titNameAct">Nombre</h4><input type="text" placeholder='UserName' className='nameAct'></input>
+            <h4 className="titCorreoAct">Email</h4><input type="email" placeholder='UserEmail' className='correoAct'></input>
+            <h4 className="titCelularAct">Celular</h4><input type="number" placeholder='UserPhone' className='celularAct'></input>
+            <h4 className="titClaveAct">Clave</h4><input type="password" placeholder='Contraseña' className='claveAct'></input>
+            <button className='actDatos' onClick="#">Actualizar</button>
+          </div>
         </div>
         <div className='forYou'>
           <div className='Separador'>
           <div className='seleccion_separador1' onClick={()=>{setContenido_foryou(<Contenido />)}}>
-            <text>Contenido</text>
+            <span>Contenido</span>
           </div>
           <div className='seleccion_separador2' onClick={()=>{console.log('se cambio')}} >
-            <text>Donaciones</text>
+            <span>Donaciones</span>
             </div>
           </div>
           {contenido_foryou}
