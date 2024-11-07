f2be219bbe688577d1e6339e2714e42dc1731ade
fmikey404 <sfuquene20@gmail.com>
2024-11-07 16:59:04 -0500

    popup detalles

--- frontend/src/components/VistaPreviaEvento.jsx
+++ frontend/src/components/VistaPreviaEvento.jsx
@@ -2,26 +2,11 @@ import '../styles/VistaPreviaEvento.module.css';
 import '../styles/HeaderPrincipal.css';
 import '../styles/detalleMatConst.module.css';
 import {useState, useEffect} from 'react';
+import Popup from "reactjs-popup"
 import PropTypes from "prop-types";
 import {ChevronLeft, ChevronRight} from "lucide-react";
 
 function VistaPreviaEvento({publicacion, empresa, materiales, idPublicacion, Inscript}) {
-    /*useEffect(() => {
-        const obtenerMateriales = async () => {
-            try {
-                const response = await fetch(`http://localhost:5000/api/materiales/${idPublicacion}`);
-                if (!response.ok) throw new Error('Error al obtener los materiales');
-                const data = await response.json();
-                console.log("Datos recibidos del servidor:", data);
-                setMateriales(data);
-            } catch (error) {
-                console.error("Error en el frontend:", error);
-            }
-        };
-
-        obtenerMateriales();
-    }, [idPublicacion]);*/
-
     return (
         <div className="contenedor-vista-previa">
             <div className="material-info">
@@ -33,18 +18,22 @@ function VistaPreviaEvento({publicacion, empresa, materiales, idPublicacion, Ins
                 <div className="contenedor-vista-previa-informacion">
                     <h2 className="titulo-material">{publicacion.titulo}</h2>
                     <p className="descripcion-material">{publicacion.descripcion}</p>
-                    <div className="detalles-material">
-                        {materiales.map((material) =>
-                            <>
-                                <h2>Material {material.id}</h2>
-                                <p><strong>Nombre:</strong> {material.nombre}</p>
-                                <p><strong>Descripción:</strong> {material.descripcion}</p>
-                                <p><strong>Cantidad:</strong> {material.cantidad}</p>
-                                <p><strong>Estado:</strong> {material.estado}</p>
-                                <p><strong>Categoría:</strong> {material.categoria}</p>
-                            </>
-                        )}
-                    </div>
+                    <Popup trigger={
+                      <div className="detalles-material">
+                      {materiales.map((material) =>
+                          <>
+                              <h2>Material {material.id}</h2>
+                              <p><strong>Nombre:</strong> {material.nombre}</p>
+                              <p><strong>Descripción:</strong> {material.descripcion}</p>
+                              <p><strong>Cantidad:</strong> {material.cantidad}</p>
+                              <p><strong>Estado:</strong> {material.estado}</p>
+                              <p><strong>Categoría:</strong> {material.categoria}</p>
+                          </>
+                      )}
+                  </div>
+                    } position={"center center"}>
+
+                    </Popup>
 
                     {/*{materiales[0].Imagenes.length > 1 && (*/}
                     {/*    <div className="navegacion-imagenes">*/}
