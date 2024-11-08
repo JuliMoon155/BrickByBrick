9053a5e46227b417f4cf2fa34f6eaa9fe23ca69b
andmoreduro <safic913@gmail.com>
2024-11-08 11:28:22 -0500

    La selección de items del header funciona correctamente

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,18 +5,15 @@ export const Header = () => {
 
     const [paso, setPaso] = useState(1);
 
-    const siguientePaso = () => { if (paso < 5) { setPaso(paso + 1); } };
-    const primerPaso = () => { if (paso > 1) { setPaso(1); } };
-
     return (  
         <div className='Encabezado'>
         <nav className='Header'>
         <ul className='navegador'>
-            <li className={paso === 1 ? 'elementoSel': 'elemento'} onClick={primerPaso}><a href="#">Inicio</a></li>
-            <li className={paso === 2 ? 'elementoSel': 'elemento'} onClick={siguientePaso}><a href="#">Calendario</a></li>
-            <li className={paso === 3 ? 'elementoSel': 'elemento'} onClick={siguientePaso}><a href="#">Comunidad</a></li>
-            <li className={paso === 4 ? 'elementoSel': 'elemento'} onClick={siguientePaso}><a href="#">Mensajes</a></li>
-            <li className={paso === 5 ? 'elementoSel': 'elemento'} onClick={siguientePaso}><a href="#">Notificaciones</a></li>
+            <li className={paso === 1 ? 'elementoSel': 'elemento'} onClick={() => {setPaso(1)}}><a href="#">Inicio</a></li>
+            <li className={paso === 2 ? 'elementoSel': 'elemento'} onClick={() => {setPaso(2)}}><a href="#">Calendario</a></li>
+            <li className={paso === 3 ? 'elementoSel': 'elemento'} onClick={() => {setPaso(3)}}><a href="#">Comunidad</a></li>
+            <li className={paso === 4 ? 'elementoSel': 'elemento'} onClick={() => {setPaso(4)}}><a href="#">Mensajes</a></li>
+            <li className={paso === 5 ? 'elementoSel': 'elemento'} onClick={() => {setPaso(5)}}><a href="#">Notificaciones</a></li>
         </ul>
         </nav>
         </div>
